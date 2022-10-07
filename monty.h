beb435e3abb53672cf6ee5e53c66fab58757c381
5isaac <isaacyegon05@gmail.com>
2022-10-07 07:07:27 -0700

    must work

--- monty.h
+++ monty.h
@@ -1,78 +1,63 @@
-#ifndef MONTY_H
-#define MONTY_H
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef __MAIN__
+#define __MAIN__
+
 #include <sys/types.h>
-#include <unistd.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <string.h>
-#include <ctype.h>
+
 /**
-** struct stack_s - doubly linked list representation of a stack (or queue)
-* @n: integer
-* @prev: points to the previous element of the stack (or queue)
-* @next: points to the next element of the stack (or queue)
-*
-* Description: doubly linked list node structure
-* for stack, queues, LIFO, FIFO Holberton project
-*/
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct stack_s
 {
-int n;
-struct stack_s *prev;
-struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
-* struct bus_s - variables -args, file, line content
-* @arg: value
-*@file: pointer to monty file
-* @content: line content
-* @lifi: flag change stack <-> queue
-* Description: carries values through the program
-*/
-typedef struct bus_s
-{
-	char *arg;
-	FILE *file;
-	char *content;
-	int lifi;
-}  bus_t;
-extern bus_t bus;
-/**
-* struct instruction_s - opcode and its function
-* @opcode: the opcode
-* @f: function to handle the opcode
-*
-* Description: opcode and its function
-* for stack, queues, LIFO, FIFO Holberton project
-*/
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
-ssize_t getstdin(char **lineptr, int file);
-char  *clean_line(char *content);
-void f_push(stack_t **head, unsigned int number);
-void f_pall(stack_t **head, unsigned int number);
-void f_pint(stack_t **head, unsigned int number);
-int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
-void free_stack(stack_t *head);
-void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
-void f_sub(stack_t **head, unsigned int counter);
-void f_div(stack_t **head, unsigned int counter);
-void f_mul(stack_t **head, unsigned int counter);
-void f_mod(stack_t **head, unsigned int counter);
-void f_pchar(stack_t **head, unsigned int counter);
-void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
-void addnode(stack_t **head, int n);
-void addqueue(stack_t **head, int n);
-void f_queue(stack_t **head, unsigned int counter);
-void f_stack(stack_t **head, unsigned int counter);
+
+void push(stack_t **, unsigned int);
+void pop(stack_t **, unsigned int);
+void swap(stack_t **, unsigned int);
+void pint(stack_t **, unsigned int);
+void add(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void pall(stack_t **, unsigned int);
+void sub(stack_t **, unsigned int);
+void add(stack_t **, unsigned int);
+void division(stack_t **, unsigned int);
+void mul(stack_t **, unsigned int);
+void mod(stack_t **, unsigned int);
+void pchar(stack_t **, unsigned int);
+void pstr(stack_t **, unsigned int);
+
+int _isdigit(int);
+void is_opcode(char *, stack_t **, unsigned int);
+char **parse(char *);
+void freestack(stack_t **);
+void check_push(stack_t **, char **, unsigned int);
+
 #endif
